9c27a2c3fe009eb3636d17e7cb10307ceb1e1e40
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2021-12-10 23:22:52 +1100

    Added docker remove

--- cmd/compose/remove.go
+++ cmd/compose/remove.go
@@ -1,9 +1,20 @@
 package compose
 
-func RemoveServices() {
+import (
+	"context"
 
+	"github.com/dkhoanguyen/ros-supervisor/models/compose"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func RemoveServices(ctx context.Context, dockerClient *client.Client, project *compose.Project) {
+	for idx := range project.Services {
+		containerID := project.Services[idx].Container.ID
+		RemoveService(ctx, dockerClient, containerID)
+	}
 }
 
-func RemoveService() {
-
+func RemoveService(ctx context.Context, dockerClient *client.Client, containerID string) {
+	dockerClient.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{})
 }
